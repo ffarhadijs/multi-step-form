3a2f5a4dfc907f6d5dfdf316c2602eac7519ae0b
farhad <rb0214192@gmail.com>
2022-12-13 18:38:48 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { CacheProvider } from "@emotion/react";
 import rtlPlugin from "stylis-plugin-rtl";
 import { prefixer } from "stylis";
 import createCache from "@emotion/cache";
-import SecondStep from "./components/secondStep/SecondStep";
-import Tabs from "./Tabs";
 const theme = createTheme({
   direction: "rtl",
 });
@@ -22,9 +20,8 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <div className="bg-gray-200 w-screen min-h-screen h-full py-10">
           <Form />
-          {/* <Tabs/> */}
         </div>
-    </ThemeProvider>
+      </ThemeProvider>
     </CacheProvider>
   );
 }
